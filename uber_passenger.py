0663736b6e0eda2f6da370851602ead238926fa1
Anastasiya Sviderska <anastasiyasviderska@Anastasiyas-MacBook-Pro.local>
2022-11-02 11:58:58 +0100

    List of orders for passenger

--- uber_passenger.py
+++ uber_passenger.py
@@ -2,7 +2,7 @@ from basic_user import BasicUser
 
 class Passenger(BasicUser):
     def draw_menu(self) -> dict:
-        menu = f"\n{'-'*40}\n\nHello Passenger {self.username}\n1. Sign Out\n2. Place an Order\n"
+        menu = f"\n{'-'*40}\n\nHello Passenger {self.username}\n1. Sign Out\n2. Place an Order\n3. List of Orders\n"
         try:
             selected_menu = int(input(menu))
             match selected_menu:
@@ -13,6 +13,12 @@ class Passenger(BasicUser):
                     destination = input('Please enter your destination: ')
                     price = input("Please enter your price: ")
                     self.uber_server.create_new_order(self.username, start_location, destination, price)  
-                    return None            
+                    return None
+                case 3:
+                    user_orders = self.uber_server.get_user_orders(self.username)
+                    for order in user_orders:
+                        print(f"from: {order['start_location']} to: {order['destination']} price: {order['price']} status: {order['order_status']}")
+                    return None
+
         except ValueError:
             return self
