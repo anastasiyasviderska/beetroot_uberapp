ab734d92c8cb6ed0f996c1bda6ce2d9c36d31036
Oana4 <sirbu.oana4@gmail.com>
2022-11-05 13:22:41 +0200

    changes in menu, payment system for driver

--- uber_driver.py
+++ uber_driver.py
@@ -1,9 +1,10 @@
 from basic_user import BasicUser
+import json
 
 
 class Driver(BasicUser):
     def draw_menu(self):
-        menu = f"\n{'-'*40}\n\nHello Driver {self.username}\n1. Sign Out\n2. Available Orders\n3. Income\n4. Exit\n"
+        menu = f"\n{'-' * 40}\n\nHello Driver {self.username}\n1. Sign Out\n2. Available Orders\n3. Income\n4. Exit\n"
         try:
             selected_menu = int(input(menu))
             match selected_menu:
@@ -11,7 +12,7 @@ class Driver(BasicUser):
                     return self.uber_server.sign_out()
                 case 2:
                     available_orders = self.uber_server.get_available_orders()
-                    print(f"\n{'-'*40}\n\n0. Back to Driver menu")
+                    print(f"\n{'-' * 40}\n\n0. Back to Driver menu")
                     for order in available_orders:
                         print(f"id: {order['id']}. from: {order['start_location']} to: {order['destination']}"
                               f" price: {order['price']} status: {order['order_status']}")
@@ -20,7 +21,8 @@ class Driver(BasicUser):
                         case 0:
                             pass
                         case _:
-                            self.raise_income(selected_id)
+                            self.raise_income_in_orders(selected_id)
+                            self.update_money_in_db()
                             self.uber_server.execute_order(selected_id)
                     return None
                 case 3:
@@ -29,3 +31,16 @@ class Driver(BasicUser):
                     exit()
         except ValueError:
             return None
+
+    def raise_income_in_orders(self, id):
+        with open('database_orders.json', 'r') as database_orders_file:
+            data = json.load(database_orders_file)
+            for order_dict in data:
+                if order_dict['id'] == id:
+                    if order_dict["order_status"] == "created":
+                        self.amount_of_money += int(order_dict['price'])
+                    else:
+                        print("This order is already executed!")
+
+
+
