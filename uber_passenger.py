5a540d74ba8ea069a44c7801bbc5c8a945a8674c
Oana4 <sirbu.oana4@gmail.com>
2022-11-05 13:23:08 +0200

    changes in menu, payment system for passengers

--- uber_passenger.py
+++ uber_passenger.py
@@ -1,10 +1,11 @@
 from basic_user import BasicUser
+import json
 
 
 class Passenger(BasicUser):
     def draw_menu(self):
         menu = f"\n{'-'*40}\n\nHello Passenger {self.username}\n1. Sign Out\n2. Place an Order\n3. " \
-               f"List of Orders\n4. Exit\n"
+               f"List of Orders\n4. Add money\n5. See balance\n6. Exit\n"
         try:
             selected_menu = int(input(menu))
             match selected_menu:
@@ -13,8 +14,17 @@ class Passenger(BasicUser):
                 case 2:
                     start_location = input('Please enter your start location: ')
                     destination = input('Please enter your destination: ')
-                    price = input("Please enter your price: ")
-                    self.uber_server.create_new_order(self.username, start_location, destination, price)  
+                    price = int(input("Please enter your price: "))
+                    if self.amount_of_money > price > 0:
+                        self.uber_server.create_new_order(self.username, start_location, destination, price)
+                        self.pay_order(price)
+                        self.update_money_in_db()
+                        print("New order created!")
+                    elif price < 0:
+                        print("Your price can't be smaller than zero!")
+                    else:
+                        print(f"You don't have enough money to place your order!"
+                              f"Your balance is {self.amount_of_money}.")
                     return None
                 case 3:
                     user_orders = self.uber_server.get_user_orders(self.username)
@@ -22,7 +32,27 @@ class Passenger(BasicUser):
                         print(f"from: {order['start_location']} to: {order['destination']} price: {order['price']} status: {order['order_status']}")
                     return None
                 case 4:
+                    self.add_money_to_balance()
+                    self.update_money_in_db()
+                case 5:
+                    print(f"Your balance is {self.amount_of_money}")
+                case 6:
                     exit()
 
         except ValueError:
             return self
+
+    def add_money_to_balance(self):
+        how_much_money = int(input("Please choose the amount of money you want to add in your account."))
+        if how_much_money > 0:
+            self.amount_of_money += how_much_money
+            print(f"Your new balance is {self.amount_of_money}")
+        else:
+            print("Your added money can't be smaller than zero!")
+
+    def pay_order(self, order_price):
+        self.amount_of_money -= order_price
+
+
+
+
