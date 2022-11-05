462c4f01f93173d8a7ab9e633487be97bdb8c25c
Oana4 <sirbu.oana4@gmail.com>
2022-11-05 13:21:59 +0200

    added amount of money functionality connected to db

--- basic_user.py
+++ basic_user.py
@@ -17,16 +17,10 @@ class BasicUser:
     def draw_menu(self) -> dict:
         pass
 
-    def raise_income(self, id):
-        with open('database_orders.json', 'r') as database_orders_file:
-            data = json.load(database_orders_file)
-            for order_dict in data:
-                if order_dict['id'] == id:
-                    self.amount_of_money += int(order_dict['price'])
-
-        with open('database.json', 'r+') as database_file:
-            data = json.load(database_file)
+    def update_money_in_db(self):
+        with open('database.json', 'r+') as users_db:
+            data = json.load(users_db)
             data[self.username]['amount_of_money'] = str(self.amount_of_money)
-            database_file.seek(0)
-            json.dump(data, database_file, indent=4)
-            database_file.truncate()
+            users_db.seek(0)
+            json.dump(data, users_db, indent=4)
+            users_db.truncate()
